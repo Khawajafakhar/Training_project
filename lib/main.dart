79831cd0b7e1560bc7c1c0366c131dda10d3e7ba
Fakhar <fakhar.oraxtech@gmail.com>
2022-09-29 12:38:14 +0500

    Basic funtions to buttons for debug console

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  void answerOne(){
+    print('Answer 1 is presssed');
+  }
+
+  void answertwo(){
+    print('Answer 2 is presssed');
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -22,19 +30,26 @@ class MyApp extends StatelessWidget {
           children: [
             Text('This is the question'),
             ElevatedButton(
-              onPressed: null,
+              onPressed: answerOne,
               child:
                   Text('Answer 1', style: TextStyle(color: Colors.amberAccent)),
             ),
             ElevatedButton(
-              onPressed: null,
+              onPressed: answertwo,
               child:
                   Text('Answer 2', style: TextStyle(color: Color.fromARGB(255, 64, 255, 223))),
             ),
             ElevatedButton(
-              onPressed: null,
+              onPressed: () => print('Answer 3 is pressed'),
               child:
                   Text('Answer 3', style: TextStyle(color: Color.fromARGB(255, 255, 64, 64))),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Answer 4 is pressed');
+              },
+              child:
+                  Text('Answer 4', style: TextStyle(color: Color.fromARGB(255, 6, 8, 0))),
             ),
           ],
         ),
