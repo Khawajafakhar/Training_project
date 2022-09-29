d67727d0e973d2dd37d02a34550641ecec5c139b
Fakhar <fakhar.oraxtech@gmail.com>
2022-09-29 12:02:29 +0500

    Working with Elevated button

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,29 @@ class MyApp extends StatelessWidget {
     // ignore: dead_code
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('This is my First app'),),
-        body: Text('Body of my first app'),
+        appBar: AppBar(
+          title: Text('This is my First app'),
+        ),
+        body: Column(
+          children: [
+            Text('This is the question'),
+            ElevatedButton(
+              onPressed: null,
+              child:
+                  Text('Answer 1', style: TextStyle(color: Colors.amberAccent)),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child:
+                  Text('Answer 2', style: TextStyle(color: Color.fromARGB(255, 64, 255, 223))),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child:
+                  Text('Answer 3', style: TextStyle(color: Color.fromARGB(255, 255, 64, 64))),
+            ),
+          ],
+        ),
       ),
     );
   }
